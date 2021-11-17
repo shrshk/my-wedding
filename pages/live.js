6b1a65590fc0245e410426f4d005f4409761f6df
shirish <shirish101k@gmail.com>
2021-11-18 01:24:19 +0530

    updated live page

--- pages/live.js
+++ pages/live.js
@@ -3,20 +3,72 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
+import Grid from '@mui/material/Grid';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Router from 'next/router';
+import * as catWaiting from '../animations/46472-lurking-cat.json';
+import Lottie from 'react-lottie'
+
+const waitingOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: catWaiting,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice'
+  }
+};
 
 export default function Live() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          This is test
-        </Typography>
-        <Button variant="text" component={Link} noLinkStyle href="/">
-          Go to the main page
-        </Button>
-        <ProTip />
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          alignItems="left"
+          justifyContent="left"
+        >
+          <Grid item xs={2}>
+            <Button variant="text" style={{ color: '#000'}} onClick={() => Router.back()}>
+              <ArrowBackIcon style={{ minWidth: '40px', height: '2em'}}/>
+            </Button>
+          </Grid>
+          <Grid item xs={10}>
+            <Typography style={{ fontFamily: 'cursive'}} gutterBottom variant="h3" component="div">
+              Sanjana & Shirish
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box sx={{ my: 4 }}>
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Grid item xs={8}>
+            <Typography style={{ fontFamily: 'FreeMono, monospace'}} gutterBottom variant="body2" color="text.secondary">
+              Stream starts @ Dec 13th, 2021 | 11:35 AM
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          alignItems="left"
+          justifyContent="left"
+        >
+          <Grid item xs={12}>
+            <Lottie options={waitingOptions}
+                    height={300}
+                    width={300}
+            />
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
